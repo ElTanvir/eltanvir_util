8b6298961294cea235243bdc127d0728de66ecbc
Tanvir Ahmed <eltanvirahmed@gmail.com>
2023-12-24 21:32:42 +0600

    clean up

--- lib/eltanvir_util.dart
+++ lib/eltanvir_util.dart
@@ -1,9 +1,8 @@
 /// App utils Designed By Eltanvir
-library eltanvir_util;
+library flutter_app_utils;
 
-export 'src/data_classes/api_state.dart';
-export 'src/data_classes/async_state.dart';
 export 'src/extensions/eltanvir_util.dart';
 export 'src/extensions/media_qurey_extensions.dart';
 export 'src/extensions/navigation.dart';
+export 'src/extensions/response_extension.dart';
 export 'src/network/utils/network_exceptions.dart';
